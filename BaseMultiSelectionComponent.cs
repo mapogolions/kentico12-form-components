0a06ccfb6dd375548bfd9c8e17381cac627a4f64
mapogolions <ivanovpavelalex45@gmail.com>
2020-04-19 15:11:22 +0500

    Initial commit

---
+++ BaseMultiSelectionComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Web.Mvc;
+using Kentico.Forms.Web.Mvc;
+
+namespace MedioClinic.Models.FormComponents
+{
+    public abstract  class BaseMultiSelectionComponent<TProperties> : FormComponent<TProperties, string>
+        where TProperties : SelectorProperties, new()
+    {
+        [BindableProperty]
+        public string[] SelectedItems { get; set; } = Array.Empty<string>();
+
+        public override string LabelForPropertyName => nameof(SelectedItems);
+
+
+        public override string GetValue() => string.Join(",", SelectedItems);
+
+        public override void SetValue(string value)
+        {
+            SelectedItems = string.IsNullOrEmpty(value)
+                ? Array.Empty<string>() : value.Split(',');
+        }
+
+        public abstract MultiSelectList Items();
+    }
+}
